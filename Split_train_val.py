998865368f0612c81d4cc958e5687eb226d5652f
TyroneHU <46662058+TyroneHU@users.noreply.github.com>
2023-03-08 14:31:54 +0800

    Create Split_train_val.py

---
+++ Split_train_val.py
@@ -0,0 +1,13 @@
+
+def main():
+  all_image_path = ''
+  all_label_path = ''
+  
+  train_image_path = ''
+  train_label_path = ''
+  
+  val_image_path = ''
+  val_label_path = ''
+  
+if __name__ == '__main__':
+  main()
